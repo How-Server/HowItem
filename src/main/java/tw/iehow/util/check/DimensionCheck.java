2298643b50bf7ed7a5b76cadf343d8ab94555424
Howie <howie0715@gmail.com>
2023-09-20 10:29:51 +0800

    Add DimensionCheck.

---
+++ src/main/java/tw/iehow/util/check/DimensionCheck.java
@@ -0,0 +1,14 @@
+package tw.iehow.util.check;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.world.World;
+
+public class DimensionCheck {
+    public static boolean isSurvival(PlayerEntity player){
+        ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+        RegistryKey<World> world = serverPlayer.getServerWorld().getRegistryKey();
+        return world.equals(World.OVERWORLD) || world.equals(World.NETHER) || world.equals(World.END);
+    }
+}
