c4cb0698ef5e709b2aee9ee09c241513ce7d6380
Howie <howie0715@gmail.com>
2023-12-08 11:15:33 +0800

    Update dependencies.

--- src/main/java/tw/iehow/util/check/ClaimCheck.java
+++ src/main/java/tw/iehow/util/check/ClaimCheck.java
@@ -7,7 +7,6 @@ import me.drex.itsours.claim.permission.node.Node;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
@@ -15,23 +14,23 @@ import java.util.Optional;
 
 public class ClaimCheck {
     public static boolean attackEntity(PlayerEntity player, Entity entity){
-        Optional<AbstractClaim> claim = ClaimList.INSTANCE.getClaimAt((ServerWorld) player.getWorld(), player.getSteppingPos());
+        Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
 
-        if (!(entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.DAMAGE_ENTITY, Node.dummy(Registries.ENTITY_TYPE, entity.getType()))) {
+        if (!(entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.DAMAGE_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()))) {
             return true;
         }
         return (entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().hasPermission(null, PermissionManager.PVP);
     }
     public static boolean useEntity(PlayerEntity player, Entity entity){
-        Optional<AbstractClaim> claim = ClaimList.INSTANCE.getClaimAt((ServerWorld) player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.INTERACT_ENTITY, Node.dummy(Registries.ENTITY_TYPE, entity.getType()));
+        Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
+        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.INTERACT_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()));
     }
     public static boolean useItem(PlayerEntity player, Hand hand){
-        Optional<AbstractClaim> claim = ClaimList.INSTANCE.getClaimAt((ServerWorld) player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.USE_ITEM, Node.dummy(Registries.ITEM, player.getStackInHand(hand).getItem()));
+        Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
+        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.USE_ITEM, Node.registry(Registries.ITEM, player.getStackInHand(hand).getItem()));
     }
     public static boolean useBlock(PlayerEntity player, World world){
-        Optional<AbstractClaim> claim = ClaimList.INSTANCE.getClaimAt((ServerWorld) player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.PLACE, Node.dummy(Registries.BLOCK, world.getBlockState(player.getBlockPos()).getBlock()));
+        Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
+        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.PLACE, Node.registry(Registries.BLOCK, world.getBlockState(player.getBlockPos()).getBlock()));
     }
 }
