c6f5db23a75bd5ad62a8944c83726b2285211b2e
Howie <howie0715@gmail.com>
2024-01-09 14:47:49 +0800

    Make sure spectator will not show particle.

--- src/main/java/tw/iehow/util/apply/PlayerParticle.java
+++ src/main/java/tw/iehow/util/apply/PlayerParticle.java
@@ -7,8 +7,10 @@ import net.minecraft.server.network.ServerPlayerEntity;
 
 public class PlayerParticle {
     public static void show(ServerPlayerEntity player, ParticleEffect particleType, double x, double y, double z, float offsetX, float offsetY, float offsetZ, float speed, int count) {
-        ParticleS2CPacket packet = new ParticleS2CPacket(particleType, false, x, y, z, offsetX, offsetY, offsetZ, speed, count);
-        player.getServerWorld().getPlayers().forEach((serverPlayer) -> sendPacket(serverPlayer, player, packet));
+        if (!player.isSpectator()){
+            ParticleS2CPacket packet = new ParticleS2CPacket(particleType, false, x, y, z, offsetX, offsetY, offsetZ, speed, count);
+            player.getServerWorld().getPlayers().forEach((serverPlayer) -> sendPacket(serverPlayer, player, packet));
+        }
     }
 
     private static void sendPacket(ServerPlayerEntity player, Entity target, ParticleS2CPacket packet){
