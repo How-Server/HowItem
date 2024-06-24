a295a39e645b621755bbd6c29af0613573ead5d8
Howie <howie0715@gmail.com>
2024-06-24 11:44:10 +0800

    update: 1.21.

--- src/main/java/tw/iehow/util/check/ClaimCheck.java
+++ src/main/java/tw/iehow/util/check/ClaimCheck.java
@@ -1,9 +1,9 @@
 package tw.iehow.util.check;
 
 import me.drex.itsours.claim.AbstractClaim;
-import me.drex.itsours.claim.ClaimList;
-import me.drex.itsours.claim.permission.PermissionManager;
-import me.drex.itsours.claim.permission.node.Node;
+import me.drex.itsours.claim.list.ClaimList;
+import me.drex.itsours.claim.flags.FlagsManager;
+import me.drex.itsours.claim.flags.node.Node;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.Registries;
@@ -16,21 +16,21 @@ public class ClaimCheck {
     public static boolean attackEntity(PlayerEntity player, Entity entity){
         Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
 
-        if (!(entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.DAMAGE_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()))) {
+        if (!(entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().checkAction(player.getUuid(), FlagsManager.DAMAGE_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()))) {
             return true;
         }
-        return (entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().hasPermission(null, PermissionManager.PVP);
+        return (entity instanceof PlayerEntity) && claim.isPresent() && !claim.get().checkAction(null, FlagsManager.PVP);
     }
     public static boolean useEntity(PlayerEntity player, Entity entity){
         Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.INTERACT_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()));
+        return claim.isPresent() && !claim.get().checkAction(player.getUuid(), FlagsManager.INTERACT_ENTITY, Node.registry(Registries.ENTITY_TYPE, entity.getType()));
     }
     public static boolean useItem(PlayerEntity player, Hand hand){
         Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.USE_ITEM, Node.registry(Registries.ITEM, player.getStackInHand(hand).getItem()));
+        return claim.isPresent() && !claim.get().checkAction(player.getUuid(), FlagsManager.USE_ITEM, Node.registry(Registries.ITEM, player.getStackInHand(hand).getItem()));
     }
     public static boolean useBlock(PlayerEntity player, World world){
         Optional<AbstractClaim> claim = ClaimList.getClaimAt(player.getWorld(), player.getSteppingPos());
-        return claim.isPresent() && !claim.get().hasPermission(player.getUuid(), PermissionManager.PLACE, Node.registry(Registries.BLOCK, world.getBlockState(player.getBlockPos()).getBlock()));
+        return claim.isPresent() && !claim.get().checkAction(player.getUuid(), FlagsManager.PLACE, Node.registry(Registries.BLOCK, world.getBlockState(player.getBlockPos()).getBlock()));
     }
 }
--- src/main/java/tw/iehow/util/placeholder/register.java
+++ src/main/java/tw/iehow/util/placeholder/register.java
@@ -6,7 +6,7 @@ import net.minecraft.util.Identifier;
 
 public class register {
     public void levelPlaceHolder() {
-        Placeholders.register(new Identifier("player", "level"), (ctx, arg) -> {
+        Placeholders.register(Identifier.of("player", "level"), (ctx, arg) -> {
             if (!ctx.hasPlayer())
                 return PlaceholderResult.invalid("No player!");
             return PlaceholderResult.value((ctx.player() != null ? ctx.player().experienceLevel : 0) + "");
