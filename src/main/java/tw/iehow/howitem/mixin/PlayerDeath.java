ef410385bc03695147faa7e6b1351f5975736f26
Howie <howie0715@gmail.com>
2024-08-01 13:41:41 +0800

    publish: 2024 Apr vip.

---
+++ src/main/java/tw/iehow/howitem/mixin/PlayerDeath.java
@@ -0,0 +1,51 @@
+package tw.iehow.howitem.mixin;
+
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import tw.iehow.howitem.util.apply.PlayerParticle;
+import tw.iehow.howitem.util.apply.PlayerSound;
+import tw.iehow.howitem.util.apply.PotionEffect;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import static tw.iehow.howitem.util.check.SlotCheck.isValid;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class PlayerDeath {
+
+    //Log for CD
+    @Unique
+    private final Map<UUID, Long> cooldown = new HashMap<>();
+
+    @Inject(method = "onDeath", at = @At("HEAD"), cancellable = true)
+    public void playerDeath(DamageSource damageSource, CallbackInfo ci) {
+        ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
+        UUID playerUuid = player.getUuid();
+        long lastUsedTime = cooldown.getOrDefault(playerUuid, 0L);
+        long currentTime = player.getWorld().getTime();
+        long interval = currentTime - lastUsedTime;
+
+        if (isValid(player.getStackInHand(Hand.OFF_HAND), "minecraft:skull_banner_pattern", 1337025)){
+            if (interval > 300){
+                player.setHealth(1.0f);
+                ci.cancel();
+                PotionEffect.add(player, StatusEffects.REGENERATION,60,4);
+                PotionEffect.add(player, StatusEffects.ABSORPTION,300,2);
+                PlayerSound.onlyPlay(player, SoundEvents.ITEM_TOTEM_USE, 0.6f, 1.0f);
+                PlayerParticle.show(player, ParticleTypes.TOTEM_OF_UNDYING, player.getX(), player.getY() + 1.0, player.getZ(), 1.5F, 1.5F, 1.5F, 0.1f, 50);
+                cooldown.put(playerUuid, currentTime);
+            }
+        }
+    }
+}
