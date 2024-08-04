92c8f21820091ca692129bf33a7c3dd135fcd2ae
Howie <howie0715@gmail.com>
2024-08-05 00:41:08 +0800

    change: player nearby can hear the sound of using totem.

--- src/main/java/tw/iehow/howitem/mixin/PlayerDeath.java
+++ src/main/java/tw/iehow/howitem/mixin/PlayerDeath.java
@@ -35,14 +35,14 @@ public abstract class PlayerDeath {
         long lastUsedTime = cooldown.getOrDefault(playerUuid, 0L);
         long currentTime = player.getWorld().getTime();
         long interval = currentTime - lastUsedTime;
-
+        //HowItem:totem
         if (isValid(player.getStackInHand(Hand.OFF_HAND), "minecraft:skull_banner_pattern", 1337025)){
             if (interval > 300){
                 player.setHealth(1.0f);
                 ci.cancel();
                 PotionEffect.add(player, StatusEffects.REGENERATION,60,4);
                 PotionEffect.add(player, StatusEffects.ABSORPTION,300,2);
-                PlayerSound.onlyPlay(player, SoundEvents.ITEM_TOTEM_USE, 0.6f, 1.0f);
+                PlayerSound.play(player, SoundEvents.ITEM_TOTEM_USE, 0.6f, 1.0f);
                 PlayerParticle.show(player, ParticleTypes.TOTEM_OF_UNDYING, player.getX(), player.getY() + 1.0, player.getZ(), 1.5F, 1.5F, 1.5F, 0.1f, 50);
                 cooldown.put(playerUuid, currentTime);
             }
