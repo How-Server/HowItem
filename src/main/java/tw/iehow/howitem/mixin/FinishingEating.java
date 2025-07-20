71241341a4350bca8b2808b32bbb9e4df8f9dc53
Howie <howie0715@gmail.com>
2025-07-20 20:49:24 +0800

    refactor: move used_bottle logic to HowFood for better organization

--- src/main/java/tw/iehow/howitem/mixin/FinishingEating.java
+++ src/main/java/tw/iehow/howitem/mixin/FinishingEating.java
@@ -7,9 +7,7 @@ import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.Node;
 import net.luckperms.api.node.types.InheritanceNode;
-import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -19,9 +17,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import tw.iehow.howitem.LegacyItem.PlasticBottle;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -31,17 +27,17 @@ import static tw.iehow.howitem.util.check.SlotCheck.isValid;
 public abstract class FinishingEating {
 
     @Inject(method = "finishUsing", at = @At("HEAD"))
-    public void afterUse(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) throws CommandSyntaxException {
-        // used_bottle
-        if(isValid(stack, Items.APPLE, 1337001)
-                || isValid(stack, Items.GOLDEN_APPLE, 1337010)
-                || isValid(stack, Items.GOLDEN_CARROT, 1337004)) {
-            PlayerEntity playerEntity = (PlayerEntity) user;
-            ItemEntity item = playerEntity.dropItem(PlasticBottle.usedBottle(), true);
-            Objects.requireNonNull(item).setPickupDelay(0);
-        }
+    public void afterUse(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+        // used_bottle move to HowFood
+//        if(isValid(stack, Items.APPLE, 1337001)
+//                || isValid(stack, Items.GOLDEN_APPLE, 1337010)
+//                || isValid(stack, Items.GOLDEN_CARROT, 1337004)) {
+//            PlayerEntity playerEntity = (PlayerEntity) user;
+//            ItemEntity item = playerEntity.dropItem(PlasticBottle.usedBottle(), true);
+//            Objects.requireNonNull(item).setPickupDelay(0);
+//        }
         // lobster
-        else if (isValid(stack, Items.ENCHANTED_GOLDEN_APPLE, 1337001)) {
+        if (isValid(stack, Items.ENCHANTED_GOLDEN_APPLE, 1337001)) {
             User user1 = LuckPermsProvider.get().getUserManager().getUser(user.getUuid());
             long expiryDuration = 0;
             for (Node node : user1.getNodes()) {
