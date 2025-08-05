cc88953b842e9796cae3f1dbdc51f48880af44cd
Howie <howie0715@gmail.com>
2025-08-05 23:12:13 +0800

    fix: modify damage calculation for players wearing specific helmet items

--- src/main/java/tw/iehow/howitem/mixin/HowHelmet.java
+++ src/main/java/tw/iehow/howitem/mixin/HowHelmet.java
@@ -1,32 +1,39 @@
 package tw.iehow.howitem.mixin;
 
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import tw.iehow.howitem.util.check.SlotCheck;
 
 import java.util.Random;
 
 @Mixin(LivingEntity.class)
 public abstract class HowHelmet {
-    @Inject(
+    @ModifyVariable(
             method = "damage",
             at = @At("HEAD"),
-            cancellable = true)
-    private void onDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+            ordinal = 0,
+            argsOnly = true
+    )
+
+    private float modifyDamage(float amount, ServerWorld world, DamageSource source) {
         LivingEntity self = (LivingEntity) (Object) this;
-        if (self instanceof PlayerEntity &&
-                (source.isOf(DamageTypes.MAGIC) || source.isOf(DamageTypes.DRAGON_BREATH) || source.isOf(DamageTypes.WITHER)
-                || source.isOf(DamageTypes.INDIRECT_MAGIC) || source.isOf(DamageTypes.SONIC_BOOM))){
-            float reduceRate = 0.1f + new Random().nextFloat() * 0.4f;
-            float reducedDamage = amount * reduceRate;
-            self.setHealth(self.getHealth() - reducedDamage);
-            cir.setReturnValue(true);
-        }
+        if (self instanceof PlayerEntity && SlotCheck.isValid(self.getEquippedStack(EquipmentSlot.HEAD), Items.SKULL_BANNER_PATTERN, 1337061)) {
+            if (source.isOf(DamageTypes.MAGIC) ||
+                    source.isOf(DamageTypes.DRAGON_BREATH) ||
+                    source.isOf(DamageTypes.WITHER) ||
+                    source.isOf(DamageTypes.INDIRECT_MAGIC) ||
+                    source.isOf(DamageTypes.SONIC_BOOM)) {
+                float reduceRate = 0.1f + new Random().nextFloat() * 0.4f;
+                return amount * (1.0f - reduceRate);
+            }
+        }return amount;
     }
 }
