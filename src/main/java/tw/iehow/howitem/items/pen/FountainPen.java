e0ae215338e2c4d7328df2f40f38d576dd817d91
Howie <howie0715@gmail.com>
2025-08-27 16:02:20 +0800

    feat: implement PenShoot class for improved shooting mechanics and particle effects

--- src/main/java/tw/iehow/howitem/items/pen/FountainPen.java
+++ src/main/java/tw/iehow/howitem/items/pen/FountainPen.java
@@ -1,30 +1,17 @@
 package tw.iehow.howitem.items.pen;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LightningEntity;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypeFilter;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
 import tw.iehow.howitem.CooldownManager;
 import tw.iehow.howitem.enums.CooldownType;
 import tw.iehow.howitem.items.BaseHowItem;
 import tw.iehow.howitem.util.apply.PlayerActionBar;
-import tw.iehow.howitem.util.apply.PlayerParticle;
 import tw.iehow.howitem.util.apply.PlayerSound;
 import tw.iehow.howitem.util.apply.PotionEffect;
-import tw.iehow.howitem.util.check.ClaimCheck;
-
-import java.util.Random;
 
 public class FountainPen extends BaseHowItem {
 
@@ -32,56 +19,10 @@ public class FountainPen extends BaseHowItem {
         super(Items.MOJANG_BANNER_PATTERN, 1337019);
     }
 
-    private static void penShoot(PlayerEntity player, float damage, int range, boolean advanced, int particleCount) {
-        Random random = new Random();
-        Vec3d direction = player.getRotationVector();
-        for (int i = 0; i < range; i++) {
-            double x = player.getX() + direction.x * i;
-            double y = player.getY() + player.getEyeHeight(player.getPose()) + direction.y * i;
-            double z = player.getZ() + direction.z * i;
-
-            BlockPos blockPos = new BlockPos((int) x, (int) y, (int) z);
-
-            if (!player.getWorld().getBlockState(blockPos).getBlock().equals(Blocks.AIR) && !player.getWorld().getBlockState(blockPos).getBlock().equals(Blocks.LIGHT)) {
-                return;
-            }
-
-            if (i > 2 && i % 2 == 0) {
-                PlayerParticle.show(player, ParticleTypes.SQUID_INK, x, y, z, 0.6f, 0.1f, 0.6f, 0.1f, particleCount);
-            }
-
-            player.getWorld().getEntitiesByType(
-                    TypeFilter.instanceOf(LivingEntity.class),
-                    new Box(x - 1, y - 1, z - 1, x + 1, y + 1, z + 1),
-                    entity -> entity != player
-            ).forEach(entity -> {
-                if (entity.isPlayer()) {
-                    PlayerEntity player1 = (PlayerEntity) entity;
-                    if (player1.isCreative() || player1.isSpectator() || ClaimCheck.canPvP(player, player.getWorld()))
-                        return;
-                }
-                entity.damage((ServerWorld) player.getWorld(), player.getDamageSources().mobProjectile(player, player), damage);
-                PlayerSound.onlyPlay(player, SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), 0.5f, 0.94f);
-                if (!advanced) {
-                    return;
-                }
-
-                PotionEffect.add(entity, StatusEffects.GLOWING, 40, 0);
-                if (random.nextInt(100) >= 10) {
-                    return;
-                }
-
-                LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, player.getWorld());
-                lightning.setPos(entity.getX(), entity.getY(), entity.getZ());
-                player.getWorld().spawnEntity(lightning);
-            });
-        }
-    }
-
     public void unsafeUse(PlayerEntity player, Hand hand) {
         long chargeCooldown = CooldownManager.get(player.getUuid(), CooldownType.PEN_CHARGED);
         if (chargeCooldown == 0) {
-            penShoot(player, 6.0F, 16, true, 20);
+            PenShoot.apply(player, 6.0F, 16, true, ParticleTypes.SQUID_INK, 20);
             PlayerSound.onlyPlay(player, SoundEvents.ENTITY_SQUID_SQUIRT, 1.0f, 1.0f);
             PotionEffect.add(player, StatusEffects.SLOWNESS, 40, 1);
             PotionEffect.add(player, StatusEffects.WEAKNESS, 40, 1);
@@ -91,7 +32,7 @@ public class FountainPen extends BaseHowItem {
 
         long cooldown = CooldownManager.get(player.getUuid(), CooldownType.PEN);
         if (cooldown == 0) {
-            penShoot(player, 6.0F, 8, false, 2);
+            PenShoot.apply(player, 6.0F, 8, false, ParticleTypes.SQUID_INK, 2);
             PlayerSound.onlyPlay(player, SoundEvents.BLOCK_HANGING_SIGN_WAXED_INTERACT_FAIL, 1.0f, 1.0f);
             CooldownManager.set(player.getUuid(), CooldownType.PEN, 4);
             PlayerActionBar.showCD(player, chargeCooldown);
---
+++ src/main/java/tw/iehow/howitem/items/pen/PenShoot.java
@@ -0,0 +1,69 @@
+package tw.iehow.howitem.items.pen;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.TypeFilter;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import tw.iehow.howitem.util.apply.PlayerParticle;
+import tw.iehow.howitem.util.apply.PlayerSound;
+import tw.iehow.howitem.util.apply.PotionEffect;
+import tw.iehow.howitem.util.check.ClaimCheck;
+
+import java.util.Random;
+
+public class PenShoot {
+        public static void apply(PlayerEntity player, float damage, int range, boolean advanced, ParticleEffect effect, int particleCount) {
+        Random random = new Random();
+        Vec3d direction = player.getRotationVector();
+        for (int i = 0; i < range; i++) {
+            double x = player.getX() + direction.x * i;
+            double y = player.getY() + player.getEyeHeight(player.getPose()) + direction.y * i;
+            double z = player.getZ() + direction.z * i;
+
+            BlockPos blockPos = new BlockPos((int) x, (int) y, (int) z);
+
+            if (!player.getWorld().getBlockState(blockPos).getBlock().equals(Blocks.AIR) && !player.getWorld().getBlockState(blockPos).getBlock().equals(Blocks.LIGHT)) {
+                return;
+            }
+
+            if (i > 2 && i % 2 == 0) {
+                PlayerParticle.show(player, effect, x, y, z, 0.6f, 0.1f, 0.6f, 0.1f, particleCount);
+            }
+
+            player.getWorld().getEntitiesByType(
+                    TypeFilter.instanceOf(LivingEntity.class),
+                    new Box(x - 1, y - 1, z - 1, x + 1, y + 1, z + 1),
+                    entity -> entity != player
+            ).forEach(entity -> {
+                if (entity.isPlayer()) {
+                    PlayerEntity player1 = (PlayerEntity) entity;
+                    if (player1.isCreative() || player1.isSpectator() || ClaimCheck.canPvP(player, player.getWorld()))
+                        return;
+                }
+                entity.damage((ServerWorld) player.getWorld(), player.getDamageSources().mobProjectile(player, player), damage);
+                PlayerSound.onlyPlay(player, SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), 0.5f, 0.94f);
+                if (!advanced) {
+                    return;
+                }
+
+                PotionEffect.add(entity, StatusEffects.GLOWING, 40, 0);
+                if (random.nextInt(100) >= 10) {
+                    return;
+                }
+
+                LightningEntity lightning = new LightningEntity(EntityType.LIGHTNING_BOLT, player.getWorld());
+                lightning.setPos(entity.getX(), entity.getY(), entity.getZ());
+                player.getWorld().spawnEntity(lightning);
+            });
+        }
+    }
+}
