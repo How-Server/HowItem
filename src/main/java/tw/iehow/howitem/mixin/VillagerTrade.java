c20e8414e3bc9ae0fd3ea8822f6ffab3da47648c
Howie <howie0715@gmail.com>
2025-08-27 16:50:07 +0800

    refactor: limit villager trading based on nearby villager count and remove hero effect

--- src/main/java/tw/iehow/howitem/mixin/VillagerTrade.java
+++ src/main/java/tw/iehow/howitem/mixin/VillagerTrade.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Mixin(VillagerEntity.class)
 public abstract class VillagerTrade {
     @Inject(method = "beginTradeWith", at = @At("HEAD"), cancellable = true)
-    public void tradeWithVillager(PlayerEntity customer, CallbackInfo ci){
+    public void limitVillager(PlayerEntity customer, CallbackInfo ci){
         VillagerEntity villager = (VillagerEntity) (Object) this;
         Box box = new Box(villager.getBlockPos()).expand(64);
         List<VillagerEntity> villagersAround = customer.getWorld().getEntitiesByType(EntityType.VILLAGER, box, e -> true);
@@ -27,7 +27,11 @@ public abstract class VillagerTrade {
             customer.sendMessage(Text.literal("無法與村民交易，周圍 64 格內超過 32 隻村民，請將村民分散，避免同時載入過多村民").formatted(Formatting.RED), true);
             ci.cancel();
         }
+    }
 
+    @Inject(method = "beginTradeWith", at = @At("HEAD"))
+    public void removeHeroEffect(PlayerEntity customer, CallbackInfo ci){
+        VillagerEntity villager = (VillagerEntity) (Object) this;
         if(villager.getCommandTags().contains("official")){
             customer.removeStatusEffect(StatusEffects.HERO_OF_THE_VILLAGE);
         }
