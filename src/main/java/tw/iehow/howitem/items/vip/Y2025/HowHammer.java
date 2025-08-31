afe6f95213149172dcde4f6d4ba19e6722d3d146
Howie <howie0715@gmail.com>
2025-09-01 01:26:58 +0800

    feat: enhance unsafeAttack method to check for solid ground before applying levitation effect

--- src/main/java/tw/iehow/howitem/items/vip/Y2025/HowHammer.java
+++ src/main/java/tw/iehow/howitem/items/vip/Y2025/HowHammer.java
@@ -1,9 +1,11 @@
 package tw.iehow.howitem.items.vip.Y2025;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import tw.iehow.howitem.CooldownManager;
 import tw.iehow.howitem.enums.CooldownType;
@@ -17,9 +19,17 @@ public class HowHammer extends BaseHowItem {
 
     public void unsafeAttack(PlayerEntity player, World world, Entity entity) {
         long cd = CooldownManager.get(player.getUuid(), CooldownType.SWORD);
-        if (player.isSneaking() && player.isOnGround() && cd == 0) {
+        boolean nearGround = false;
+
+        for (int i = 1; i <= 2; i++) {
+            BlockPos checkPos = player.getBlockPos().down(i);
+            BlockState blockState = world.getBlockState(checkPos);
+            if (!blockState.isAir() && blockState.isSolidBlock(world, checkPos)) nearGround = true;
+        }
+
+        if (player.isSneaking() && nearGround && cd == 0) {
             PotionEffect.add(player, StatusEffects.LEVITATION, 3, 59);
-            CooldownManager.set(player.getUuid(), CooldownType.SWORD, 10, player, player.getMainHandStack());
+            CooldownManager.set(player.getUuid(), CooldownType.SWORD, 5, player, player.getMainHandStack());
         }
     }
 }
