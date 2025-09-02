e1d2e006e1f69d15dddbeda25d89f0a508211219
Howie <howie0715@gmail.com>
2025-09-02 11:58:54 +0800

    refactor: simplify CooldownManager set method by removing player and stack parameters

--- src/main/java/tw/iehow/howitem/CooldownManager.java
+++ src/main/java/tw/iehow/howitem/CooldownManager.java
@@ -1,8 +1,5 @@
 package tw.iehow.howitem;
 
-import net.minecraft.entity.player.ItemCooldownManager;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import tw.iehow.howitem.enums.CooldownType;
 
@@ -22,13 +19,6 @@ public class CooldownManager {
     public static void set(UUID uuid, CooldownType type, long cd) {
         cooldown.computeIfAbsent(uuid, k -> new HashMap<>());
         cooldown.get(uuid).put(type, server.getTicks() + cd);
-    }
-
-    // add vanilla item cooldown
-    public static void set(UUID uuid, CooldownType type, long cd, PlayerEntity player, ItemStack stack) {
-        ItemCooldownManager itemCooldownManager = player.getItemCooldownManager();
-        itemCooldownManager.set(stack, (int) cd);
-        set(uuid, type, cd);
     }
 
     public static void setServer(MinecraftServer actualServer) {
